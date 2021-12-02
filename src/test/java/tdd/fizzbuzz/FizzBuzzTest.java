fcdbe4ad492a7106fb7a7faac05b4953e8b17c39
michelle <81959777+michelle912@users.noreply.github.com>
2021-12-02 13:57:07 +0800

    add test case for normal numbers

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,22 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+
+    @Test
+    public void should_return_number_when_countOff_given_normal_number() {
+        // given
+        int number = 1;
+
+        // when
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String result = fizzBuzz.countOff(number);
+
+        // then
+        assertEquals("1", result);
+    }
+
 }
