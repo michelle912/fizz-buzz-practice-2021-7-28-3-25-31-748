1b6a60763994aeb2b90d97a9e67c9f282e709b59
michelle <81959777+michelle912@users.noreply.github.com>
2021-12-02 14:07:15 +0800

    implement countOff function

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,4 +1,26 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
+
+    public String countOff(int number) {
+        String output = "";
+        // Fizz
+        if (number % 3 == 0) {
+            output += "Fizz";
+        }
+        // Buzz
+        if (number % 5 == 0) {
+            output += "Buzz";
+        }
+        // Whiz
+        if (number % 7 == 0) {
+            output += "Whiz";
+        }
+        // normal case
+        if (output.isEmpty()) {
+            return String.valueOf(number);
+        }
+        return output;
+    }
+
 }
